a3c46238c64f3c68eab374981076db46ad47f376
Ismail Allouch <allouchsmail92@gmail.com>
2025-06-27 20:53:03 +0100

    initial commit

--- Recommendation-books-final0/Recommendation-books-Eventiclub/Recommendation-books-Eventi/backend/config/db.js
+++ Recommendation-books-final0/Recommendation-books-Eventiclub/Recommendation-books-Eventi/backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://<allouchsmail92>:<42XUkKFVhEpcJCXJ>@cluster0.rrsrp8u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://allouchsmail92:42XUkKFVhEpcJCXJ@cluster0.rrsrp8u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
